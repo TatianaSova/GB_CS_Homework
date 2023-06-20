c7db5f7d510424deb932d25fc75436c26bb62853
Tatiana Sova <tsovauhu@gmail.com>
2023-06-20 19:26:55 +0300

    задание 8

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите длину: ");
+string a = Console.ReadLine() ?? "";
+int N = int.Parse(a);
+    
+for (int i = 1; i <= N; i++)
+if (i%2 == 0)
+{
+     Console.WriteLine($"{i}");
+}
