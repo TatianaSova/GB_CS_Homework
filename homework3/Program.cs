c59c5c5b92c1d9dd094efbf48736144584fdb6c2
Tatiana Sova <tsovauhu@gmail.com>
2023-06-20 20:04:38 +0300

    homework3 project added

---
+++ homework3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите первое целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int a = int.Parse(userInput1);
+Console.Write("Введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int b = int.Parse(userInput2);
+ 
+if (a==b)
+{
+ Console.WriteLine($"a={a}, b={b} -> no");
+}
+else
+{
+ Console.WriteLine($"a={a}, b={b} -> max={b}");
+}
