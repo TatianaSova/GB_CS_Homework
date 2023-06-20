eea3db5b9d4dfd41a20b8a2bdbf5a3255ebd5361
Tatiana Sova <tsovauhu@gmail.com>
2023-06-20 20:20:43 +0300

    задание 6

---
+++ homework5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int a = int.Parse(userInput);
+
+if (a%2==0)
+{
+    Console.WriteLine ($"a={a%2} -> да");
+}
+else
+{
+    Console.WriteLine ($"a={a%2} -> нет");
+}
