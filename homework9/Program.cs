9dd5dff374cc672cf0093ae5530b34414a6fb12f
Tatiana Sova <tsovauhu@gmail.com>
2023-06-22 14:58:08 +0300

    homework9 project added

---
+++ homework9/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+int a = 0, num = number;
+while (number > 0) {a = a * 10 + number % 10; number /= 10;}
+if (num == a)
+{
+ Console.WriteLine($"да");
+}
+else
+{
+ Console.WriteLine($"нет");
+}
