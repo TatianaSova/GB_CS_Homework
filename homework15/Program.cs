741b671deb7ba9232b20ce9d2c841db0344b09b3
Tatiana Sova <tsovauhu@gmail.com>
2023-07-04 12:45:08 +0300

    homework15 project added

--- homework15/Program.cs
+++ homework15/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите размер массива");
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
